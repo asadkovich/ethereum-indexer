7256af2587c6afef55db00f79aa8badfd83b0290
AlexeySadkovich <sadkovich01@mail.ru>
2022-12-20 10:22:25 +0300

    chore(rpc): update error handling

--- src/rpc.rs
+++ src/rpc.rs
@@ -2,8 +2,8 @@ use web3::{transports::WebSocket, Web3};
 
 pub type RPC = Web3<WebSocket>;
 
-pub async fn connect(rpc_url: &str) -> Result<RPC, &'static str> {
-    let transport = WebSocket::new(rpc_url).await.unwrap();
+pub async fn connect(rpc_url: &str) -> Result<RPC, crate::Error> {
+    let transport = WebSocket::new(rpc_url).await?;
     let web3 = Web3::new(transport);
 
     Ok(web3)
